123648877420c1aec2ce3c91aa6a285bd1e17e3b
ramyamut <51800150+ramyamut@users.noreply.github.com>
2020-06-05 11:59:12 -0400

    added scripts for resection pipeline

--- pipeline/resection_pipeline.sh
+++ pipeline/resection_pipeline.sh
@@ -1,4 +1,4 @@
-# This code uses the given pre-operative and post-operative images and outputs the resection volumes by brain region as a json file
+# This code uses the given pre-operative and post-operative images and outputs the resection percentages by brain region as a json file
 # and prints percent volume resected for each ROI in the brain
 
 # Usage: resection_pipeline.sh patient_name preop.nii postop.nii output_dir
@@ -11,8 +11,10 @@
 mask_name="${1}_predicted_mask.nii.gz"
 
 # generate a predicted mask NIFTI file for the post-operative image
-python3 generate_mask.py ${3} ${4} ${mask_name}
+python3 ./scripts/generate_mask.py ${3} ${4} ${mask_name}
 
 mask_file="${4}/${mask_name}"
 atlas_file="analysis/${1}/atlas2post_AAL116_origin_MNI_T1.nii.gz"
-python3 calculate_resected_volumes.py ${mask_file} ${atlas_file} ${4}
+
+# generate a json file that calculates the percentages by brain region
+python3 ./scripts/calculate_resected_volumes.py ${mask_file} ${atlas_file} ${4}
